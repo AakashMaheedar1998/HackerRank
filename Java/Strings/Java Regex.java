f28da1032e9c0c96aaec27a1cabe9ed7c1f95308
Aakash <45240744+AakashMaheedar@users.noreply.github.com>
2019-06-06 14:59:23 +0530

    Add files via upload

---
+++ Java/Strings/Java Regex.java
@@ -0,0 +1,29 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+
+/*
+    0-199   ->[0-1]?\\d{1,2}
+    200-249 ->2[0-4]\\d
+    250-255 ->25[0-5]
+
+*/
+class MyRegex
+{
+    String num ="([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+    String pattern=num+"."+num+"."+num+"."+num;
+}
